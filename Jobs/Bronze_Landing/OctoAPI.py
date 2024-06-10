0f9ee06641ad678e0660c801a3798c448f0ed1ca
Chris Hinds <c.hinds09@googlemail.com>
2024-06-10 21:22:48 +0100

    Adding docstring

--- Jobs/Bronze_Landing/OctoAPI.py
+++ Jobs/Bronze_Landing/OctoAPI.py
@@ -240,7 +240,29 @@ class OctopusApi(ABC):
     
 
     def dataframe_writer(self, spark, container_name, storage_account_name):
-        print('dataframewriter')
+        """
+        Writes consumption data to Azure Blob Storage in Parquet format.
+    
+        This method continuously fetches consumption data from a URL, converts the data into a Spark DataFrame, and writes
+        the DataFrame to Azure Blob Storage in Parquet format. The process continues until there are no more URLs to fetch
+        data from.
+    
+        Args:
+            spark (SparkSession): The SparkSession object used to create DataFrames and write data.
+            container_name (str): The name of the Azure Blob Storage container.
+            storage_account_name (str): The name of the Azure Storage account.
+    
+        Raises:
+            Exception: If there is an error writing the DataFrame to Azure Blob Storage.
+    
+        Notes:
+            - This method expects the instance to have `self.con_url` set to the initial URL to fetch data from.
+            - The method updates `self.con_url` and `self.consumption_dict` with each fetch.
+            - It assumes `self.consumption_get` is a method that fetches data and updates `self.con_url` and `self.consumption_dict`.
+            - The method also assumes `self.elec_details` contains the serial number of the electricity meter.
+            - The method requires PySpark to be configured to access Azure Blob Storage using `wasbs://` protocol.
+    """
+        
         while self.con_url is not None:
             
             self.consumption_dict, self.con_url = self.consumption_get( self.con_url)
