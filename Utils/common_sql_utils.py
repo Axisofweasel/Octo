63d84c625963c215530c63efe41e86b6810acf78
Chris Hinds <c.hinds09@googlemail.com>
2024-07-25 13:35:01 +0100

    updating class structures

---
+++ Utils/common_sql_utils.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+from datetime import datetime
+import urllib.parse
+from abc import ABC, abstractmethod
+
+#Set environment variables
+
+class SqlUtil(ABC):
+    
+    def __init__(self, server:str =None, database:str =None, username:str =None, password:str=None, encrypt: str ='no'):
+        
+        self.server = server
+        self.database = database
+        self.username = username
+        self.password = password
+
+        if not all([self.server, self.database, self.username, self.password]):
+            raise ValueError('All Database connection parameters must be provided')
+
+        self.params = urllib.parse.quote_plus(
+                f"DRIVER={{ODBC Driver 18 for SQL server}};"
+                f"server={server};"
+                f"database={database};"
+                f"UID={username};"
+                f"PWD={password};"
+                f"encrypt={encrypt}"
+            )
+        
+        self.engine = create_engine(f'mssql+pyodbc:///?odbc_connect={params}')
+        
+    @abstractmethod
+    def setup_logging(self, schema: str, table: str, if_exists: str = 'append', index: bool = False, columns: list = None):
+    pass
+
+    @abstractmethod
+    def log_message(self, log_messages: list):
+    pass
+
+    @abstractmethod
+    def write_logging(self):
+    pass
+    
+class SqlLogger(SqlUtil):
+    
+    def __init__(self, server:str =None, database:str =None, username:str =None, password:str=None, encrypt: str ='no'):
+        super().__init__(server, database, username, password, encrypt)
+
+        self.logging = []
+        self.logging_df = None
+        self.schema = None
+        self.table = None
+        self.if_exists = 'append'
+        self.index = False
+        
+        return
+    
+    def setup_logging(self, schema: str, table: str, if_exists: str = 'append', index: bool = False, columns: list = None):
+        
+        self.schema=schema
+        self.table=table
+        self.if_exists = if_exists
+        self.index = index 
+        self.logging_df = pd.DataFrame(columns=columns)
+        self.log_columns
+
+    
+    def log_message(self,log_messages:list):
+        
+        if len(log_messages)!= len(self.logging_df.index):
+            raise ValueError(f'Logging message list must have {len(self.logging_df.index)} items')
+        
+        new_df = pd.DataFrame(columns=self.columns, data=log_messages)
+        self.logging_df =pd.concat(self.logging_df, new_df,ignore_index=False)
+
+    
+    def write_logging(self):
+        
+        if self.logging_df is None or self.logging_df.empty
+            print('No logs to write.')
+        
+        try:
+            rows = len(self.logging_df.index)
+            self.logging_df.to_sql(name=self.schema, con=self.engine, if_exists=self.if_exists, index=self.index)
+            print('Logging succesful:{rows} inserted to {self.schema}.{self.table} ')
+        except Exception as e:
+            print(f'Error writing logs to database:{str(e)}')
+            raise
+        
+        return
+        
+        
+
--- Utils/spark_builder.py
+++ Utils/spark_builder.py
@@ -7,52 +7,13 @@ from pyspark.sql import SparkSession
 
 class SparkManager:
     
-    def __init__(self):
-
-        self.jar_dir = None
-        self.container_name = None
-        self.jar_paths = None
-        self.storage_account_name = None
-        self.sas_key = None
-        self.JDBC_SERVER = None
-        self.JDBC_DATABASE = None
-        self.JDBCcoconnectionString = None
-        self.spark = None
-        
-        return
+    def __init__(self, storage_account_name: str = None, sas_key: str = None, jar_dir: str = None, container_name: str = None, print_jars: bool = False):
     
-    def wasbs_config(self, storage_account_name=None, sas_key=None):
-        """
-        Initialise Spark Class and pass storage account name and sas key into object.
-
-        Args:
-            storage_account (string, optional): storage account to be written or read from current spark session. When left as Default None will pull local Env 'BLOBSTORAGE'
-            sas_key (_type_, optional): storage account SAS key for storage account in current spark session. When left as Default None will pull local Env 'BLOBSASKEY'
-        """
-        self.storage_account_name = storage_account_name or os.getenv('BLOBSTORAGE')
-        self.sas_key = sas_key or os.getenv('BLOBSASKEY')
-        
-        if not self.storage_account_name:
-            ValueError("Storage account name must be provided either as an argument or through the 'BLOBSTORAGE' environment variable.")
-        
-        if not self.sas_key:
-            ValueError("SAS key must be provided either as an argument or through the 'BLOBSASKEY' environment variable.")
-            
-        return 
-        
-    def spark_session_builder_jar(self, jar_dir=None, container_name=None, print_jars:bool = False):
-        """
-        Create the SparkSession while passing the local Jar Directory either as path or from .env file from JAR_DIR variable
-        
-        Args:
-            jar_dir (string, optional): path to directory holding spark session Jars. Defaults to None. When left as Default will pull local Env 'JAR_DIR'
-            container_name (string, optional): Azure Blob Storage container name. Defaults to None.
-        Returns:
-            SparkSession: New Spark Session with local Jar Files installed.
-        """
-        self.jar_dir = jar_dir or os.getenv('JAR_DIR')
+        self.storage_account_name = storage_account_name
+        self.sas_key = sas_key
+        self.jar_dir = jar_dir
         self.container_name = container_name
-            
+ 
         if os.path.exists(self.jar_dir) == False:
             raise FileNotFoundError('Path does not Exist')
         
@@ -64,10 +25,13 @@ class SparkManager:
         self.spark = (SparkSession.builder
                                     .appName("Azure Blob Storage Access")
                                     .config("spark.jars", self.jar_paths)
-                                    .getOrCreate()
                                     )
 
         if self.container_name:
+            if not self.storage_account_name:
+                ValueError("Storage account name must be provided either as an argument or through the 'BLOBSTORAGE' environment variable.")
+            if not self.sas_key:
+                ValueError("SAS key must be provided either as an argument or through the 'BLOBSASKEY' environment variable.")
             self.spark = self.spark.conf.set(
                 f"fs.azure.sas.{self.container_name}.{self.storage_account_name}.blob.core.windows.net",
                 f"{self.sas_key}"
@@ -76,9 +40,9 @@ class SparkManager:
         if print_jars == True:
             [print(jar) for jar in list(self.jar_paths.split(','))]
         
-        return self
+        return self.spark.getOrCreate()
     
-    def write_to_wasbs(self):
+    def generate_wasbs_string(self):
         """Generates Stringpath to write to azure storage using WASBS protocol.
 
         Returns:
@@ -88,75 +52,66 @@ class SparkManager:
 
         return stringpath
     
-    def jdbc_config(self, SERVER :str = None, DATABASE:str = None):
+    
+class SparkJdbcWriter:
+    
+    def __init__(self, server :str = None, database:str = None):
         """ Sets up the necessary connection string for writing to SQL server
 
         Args:
-            SERVER (str, optional): target server name. Defaults to None.
-            DATABASE (str, optional): target database name. Defaults to None.
+            server (str, optional): target server name. Defaults to None.
+            database (str, optional): target database name. Defaults to None.
         """
         
-        self.JDBC_SERVER = SERVER
-        self.JDBC_DATABASE = DATABASE
+        self.JDBC_server = server
+        self.JDBC_database = database
 
-        self.JDBCcoconnectionString = f'jdbc:sqlserver://{SERVER}:1433;database={DATABASE}'
+        self.JDBCcoconnectionString = f'jdbc:sqlserver://{server}:1433;database={database}'
         
         return
     
-    def jdbc_writer(self, DATAFRAME = None, TABLE: str = None, USERNAME: str = None, PASSWORD: str = None, TRUNCATE: str = 'true', WRITEMODE: str = 'overwrite', ENCRYPT: str = 'false'):
+    def jdbc_writer(self, dataframe = None, table: str = None, username: str = None, password: str = None, truncate: str = 'true', writemode: str = 'overwrite', encrypt: str = 'false'):
     
-        if DATAFRAME is None:
+        if dataframe is None:
             ValueError('DATAFRAME variable must be a spark dataframe')
-        if TABLE is None:
-            ValueError(f'TABLE variable must be an existing table name in {self.JDBC_SERVER}{self.JDBC_DATABASE} string format')
-        if USERNAME is None:
-            ValueError(f'USERNAME variable must be a valid username for {self.JDBC_SERVER}{self.JDBC_DATABASE} as string')
-        if PASSWORD is None:
-            ValueError(f'PASSWORD variable must be a valid username for {self.JDBC_SERVER}{self.JDBC_DATABASE} as string')
-        if TRUNCATE in ['true','false']:
-            ValueError(f'TRUNCATE value must be true or false')
-        if WRITEMODE in ['overwrite','append','error','errorifexists', 'ignore']:
-            ValueError(f'WRITEMODE must be a string representation of the following: overwrite, append, error, errorifexists, ignore')
-        if TRUNCATE in ['true','false']:
-            ValueError(f'TRUNCATE must be a string value of true or false')
-        if ENCRYPT in ['true','false']:
-            ValueError(f'TRUNCATE must be string value of true or false')
+        if table is None:
+            ValueError(f'table variable must be an existing table name in {self.JDBC_server}{self.JDBC_database} string format')
+        if username is None:
+            ValueError(f'username variable must be a valid username for {self.JDBC_server}{self.JDBC_database} as string')
+        if password is None:
+            ValueError(f'password variable must be a valid username for {self.JDBC_server}{self.JDBC_database} as string')
+        if truncate in ['true','false']:
+            ValueError(f'truncate value must be true or false')
+        if writemode in ['overwrite','append','error','errorifexists', 'ignore']:
+            ValueError(f'writemode must be a string representation of the following: overwrite, append, error, errorifexists, ignore')
+        if truncate in ['true','false']:
+            ValueError(f'truncate must be a string value of true or false')
+        if encrypt in ['true','false']:
+            ValueError(f'truncate must be string value of true or false')
         
         try:
-            (DATAFRAME
+            (dataframe
              .write
              .format('jdbc')
              .option('url', f'{self.connectionString}')
-             .option('dbtable', f'{TABLE}')
-             .option('user', f'{USERNAME}')
-             .option('password', f'{PASSWORD}')
-             .option('encrypt', f'{ENCRYPT}')
-             .option('truncate', f'{TRUNCATE}')
-             .mode(f'{WRITEMODE}')
+             .option('dbtable', f'{table}')
+             .option('user', f'{username}')
+             .option('password', f'{password}')
+             .option('encrypt', f'{encrypt}')
+             .option('truncate', f'{truncate}')
+             .mode(f'{writemode}')
              .save())
 
             count = DATAFRAME.count()
 
         except ConnectionError() as e:
-            print(f'Failed to connect to {self.JDBC_DATABASE}.{TABLE}:{e}')
+            print(f'Failed to connect to {self.JDBC_database}.{table}:{e}')
             raise
         except ConnectionRefusedError() as e:
-            print(f'Connection refused for {self.JDBC_DATABASE}.{TABLE}: {e}')
+            print(f'Connection refused for {self.JDBC_database}.{table}: {e}')
             raise
         except PermissionError() as e:
-            print(f'Incorrect permissions to write to {self.JDBC_DATABASE}.{TABLE}: {e}')
+            print(f'Incorrect permissions to write to {self.JDBC_database}.{table}: {e}')
             raise
         
-        return print(f'{count} rows written to {self.JDBC_DATABASE}.{TABLE} as {WRITEMODE}')
-    
-    
-    def kill_spark_session(self):
-        """
-        Kills the current spark session.
-        """        
-        self.spark.stop()
-        
-        print('Spark Session Azure Blob Storage Access is stopped')
-        
-        return
-    
+        return print(f'{count} rows written to {self.JDBC_database}.{table} as {writemode}')
